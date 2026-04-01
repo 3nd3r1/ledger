fcb1b08e45d77a9b83afdcba05a338d84580bc39
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-01 12:17:59 +0300

    fix(prep): Fix the text creation

--- preparation.py
+++ preparation.py
@@ -1,17 +1,15 @@
-import logging
-
 import pandas as pd
 
 
 def read_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path, encoding="latin-1")
-    logging.debug(f"Loaded {len(df)} rows and {len(df.columns)} columns")
+    df["Order Date"] = pd.to_datetime(df["Order Date"])
     return df
 
 
 def create_row_text(row: pd.Series) -> str:
     return (
-        f"Order {row['Order Id']} on {row['Order Date']}: "
+        f"Order {row['Order ID']} on {row['Order Date']}: "
         f"Customer {row['Customer Name']} from {row['City']}, {row['State']}, {row['Region']} "
         f"ordered {row['Quantity']} '{row['Product Name']}' "
         f"with ship mode {row['Ship Mode']}, generating {row['Sales']} in sales and {row['Profit']} in profit."
@@ -32,10 +30,14 @@ def create_month_text(row: pd.Series) -> str:
 
 
 def create_month_texts(df: pd.DataFrame) -> list[str]:
-    month_rows = df.groupby(pd.Grouper(key="Order Date", freq="M")).agg(
-        total_sales=("Sales", "sum"),
-        total_orders=("Order ID", "nunique"),
-        total_profit=("Profit", "sum"),
+    month_rows = (
+        df.groupby(df["Order Date"].dt.to_period("M"))
+        .agg(
+            total_sales=("Sales", "sum"),
+            total_orders=("Order ID", "nunique"),
+            total_profit=("Profit", "sum"),
+        )
+        .reset_index()
     )
 
     return [create_month_text(month_row) for _, month_row in month_rows.iterrows()]
@@ -51,10 +53,14 @@ def create_region_text(row: pd.Series) -> str:
 
 
 def create_region_texts(df: pd.DataFrame) -> list[str]:
-    region_rows = df.groupby("Region").agg(
-        total_sales=("Sales", "sum"),
-        total_orders=("Order ID", "nunique"),
-        total_profit=("Profit", "sum"),
+    region_rows = (
+        df.groupby("Region")
+        .agg(
+            total_sales=("Sales", "sum"),
+            total_orders=("Order ID", "nunique"),
+            total_profit=("Profit", "sum"),
+        )
+        .reset_index()
     )
 
     return [create_region_text(region_row) for _, region_row in region_rows.iterrows()]
